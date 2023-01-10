1099ad7ae0dc203738ac32a56ab2bd15e53d1324
3talks <3talks123@gmail.com>
2023-01-10 14:46:52 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+var mainListDiv = document.getElementById("mainListDiv"),
+    mediaButton = document.getElementById("mediaButton");
+
+mediaButton.onclick = function () {
+    
+    "use strict";
+    
+    mainListDiv.classList.toggle("show_list");
+    mediaButton.classList.toggle("active");
+    
+};
+
+
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("mainListDiv").style.top = "0";
+    } else {
+      document.getElementById("mainListDiv").style.top = "-100px";
+    }
+    prevScrollpos = currentScrollPos;
+  };
+
